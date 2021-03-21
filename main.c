07f12d8a5ea67c23c271754bce8e7e8d1fe6abc2
edmond120 <edmondwong888@msn.com>
2021-03-21 19:10:41 -0400

    Add help message functionality to main.c

--- main.c
+++ main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 #include "commands.h"
 
+static void print_help_message() {
+	printf("Usage: profiler <command> [<command args>]\n\n");
+	printf("Command List\n\n");
+	for (int i = 0; i < commands_length; i++) {
+		printf("%s\n\n", command_descriptions[i]);
+	}
+}
+
 int main(int argc, char *argv[]) {
-	return select_command(argc, argv);
+	if (argc < 2) {
+		print_help_message();
+		return 0;
+	}
+	int error_code = select_command(argc, argv);
+	if (error_code == -1) {
+		printf("Error: command not found\n");
+	}
+	return error_code;
 }
