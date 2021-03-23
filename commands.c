9b21e6832718b20b0a334ac58b84dd890dda920f
edmond120 <edmondwong888@msn.com>
2021-03-22 23:58:08 -0400

    Change neighborhood command desc show INCOMPLETE

--- commands.c
+++ commands.c
@@ -19,6 +19,7 @@ int commands_length = sizeof(commands) / sizeof(char *);
 
 char * command_descriptions[] = {
 	"neighborhood <profile> <filename>\n"
+	"\t[[ !!INCOMPLETE!! ]]\n"
 	"\t<profile> is either Imax, Imin, Emax, or Emin\n"
 	"\t<filename> is in g6 format, showg from nauty is used behind the\n"
 	"\tscenes to read the file. The profile for each graph in\n"
