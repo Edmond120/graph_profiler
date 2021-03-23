6e947334d2afdb16b5a1edd9e9d659c8224a8ced
edmond120 <edmondwong888@msn.com>
2021-03-23 19:51:25 -0400

    Add counting sort implementation in array_utils

--- array_utils.c
+++ array_utils.c
@@ -10,3 +10,18 @@ void print_int_array(int length, int *array) {
 	}
 	putchar('\n');
 }
+
+void int_counting_sort(int bound, int length, int *array) {
+	int counts[bound] = {0};
+	for (int i = 0; i < length; i++) {
+		counts[array[i]] += 1;
+	}
+	int index = length - 1;
+	int n = bound - 1;
+	while (n >= 0 && index >= 0) {
+		for (int i = 0; i < counts[n]; i++) {
+			array[index--] = n;
+		}
+		n--;
+	}
+}
--- array_utils.h
+++ array_utils.h
@@ -3,4 +3,8 @@
 
 void print_int_array(int length, int *array);
 
+/* Sorts array using a counting sort. The integers in the array must be
+ * between 0 and bound (exclusive) */
+void int_counting_sort(int bound, int length, int *array);
+
 #endif
