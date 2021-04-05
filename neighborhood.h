9a393f588da47aa318e271a40be3b48289374222
edmond120 <edmondwong888@msn.com>
2021-04-05 11:46:38 -0400

    doc: improve a comment in neighborhood.h

--- neighborhood.h
+++ neighborhood.h
@@ -23,6 +23,8 @@ Profile * create_neighborhood_profile_sorted(Graph *graph, N_profile_type);
 /* Returns the unsorted degree sequence of all the nodes around the
  * node at node_index. Writes the length of the sequence to the address
  * in the argument "length".
+ * The return array is essentially the degree sequence of the node's
+ * exclusive neighborhood.
  * A call to free should be made on the return value. */
 int * neighborhood_degree_sequence(Graph *graph, int node_index, int *length);
 
